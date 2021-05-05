a7ef0e1ca9ae25d4cfbed40fcd2093dd8cc5505d
kivancm <83455807+kivancm@users.noreply.github.com>
2021-05-05 19:40:19 +0300

    Update ICustomerCheckService.java

--- 4.gunOdev2/src/Abstract/ICustomerCheckService.java
+++ 4.gunOdev2/src/Abstract/ICustomerCheckService.java
@@ -4,7 +4,7 @@ import Entities.Customer;
 
 public interface ICustomerCheckService {
 /*ici bos metot tanimlarini interfacede yapariz
- classda metotlarý uygulatýrýz yani metotlarin icini doldururuz
+ classda metotlari uygulatiriz yani metotlarin icini doldururuz
 */
 	boolean CheckIfRealPerson(Customer customer);
 }
